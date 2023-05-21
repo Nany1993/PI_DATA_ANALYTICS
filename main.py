8d39c4d7e42b86a68102c34ed19fc2fd51c92f08
Nany1993 <anamariagarcia093@gmail.com>
2023-05-21 18:25:43 -0500

    Actualizacion

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_accidentes = pd.read_csv("df_accidente2.csv", sep=",")
+
+st.title("DASHBOARD ACCIDENTES AÉREOS")
 
